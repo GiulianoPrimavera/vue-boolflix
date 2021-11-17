d9f2222fc42881091f3c77b0e8f3cc2e8eb919d9
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-11-17 15:21:15 +0100

    setup main project files

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="main_container">
+      <ul>
+
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -22,7 +25,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
