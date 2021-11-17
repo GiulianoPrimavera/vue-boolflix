d3e01efd96ad5e2fa2f8d204c0dcf0acb9c47e33
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-11-17 16:03:59 +0100

    adds filter that shows a list of movies names based on the searched word

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,73 @@
 <template>
   <div id="app">
     <div class="main_container">
+      <input
+        type="text"
+        placeholder="cerca un film"
+        v-model="searchedString"
+        @keyup.enter="startSearching"
+      />
+      <button @click="startSearching">cerca</button>
       <ul>
-        <li>Hello wordl</li>
+        <li v-for="(movie, i) in movies" :key="i">
+          {{ movie.original_title }}
+        </li>
       </ul>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {}
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      apiKey: "cd46b1fc41160a5e0f5ace06fece242a",
+      movies: [],
+      searchedString: "",
+    };
+  },
+  methods: {
+    //eseguo la richiesta da axios all'api e popolo l'array "movies" ogni volta che premo enter o clicco su cerca
+    startSearching() {
+      //svuoto l'array all'inizio della funzione
+      this.movies = [];
+
+      //eseguo la chiamata axios
+      axios
+        //come primo elemento della funzione get metto la prima parte necessaria per la ricerca di films (data dalla documentazione dell'api)
+        .get("https://api.themoviedb.org/3/search/movie?", {
+          //come secondo elemento del get inserisco params (un oggetto nel quale vengono specificati dei parametri, axios "traduce" questi parametri in stringa di ricerca)
+          params: {
+            //this.apiKey è la chiave data dall'api
+            api_key: this.apiKey,
+            //this.searchedString è un v-model inserito nell'input di testo, query è il secondo parametro necessario per la ricerca dei film
+            query: this.searchedString,
+          },
+        })
+        .then((resp) => {
+          //prendo l'array di risposta dato dall'api e lo inseriso nel mio array movies
+          this.movies.push(...resp.data.results);
+        });
+      console.log("array di film", this.movies);
+    },
+  },
+  mounted() {
+    /* axios
+    .get("https://api.themoviedb.org/3/search/movie?", {
+      params: {
+        api_key: this.apiKey,
+        query: this.searchedString
+    }})
+    .then(resp => {
+      this.movies.push(...resp.data.results) 
+    });
+    console.log("array di prova", this.movies); */
+  },
+};
 </script>
 
 <style lang="scss">
