b41d097ba77a3938fdf910e03e1e0d146036b736
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-11-18 14:46:42 +0100

    adds an api call that prints the tv shows with the movies

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,44 @@
       />
       <button @click="startSearching">cerca</button>
       <ul>
+        <h2>Movies</h2>
         <li v-for="(movie, i) in movies" :key="i">
-          <p>titolo: {{ movie.original_title }}</p>
-          <p>titolo originale: {{ movie.title }}</p>
+          <p><strong>titolo:</strong> {{ movie.title }}</p>
+          <p><strong>titolo originale:</strong> {{ movie.original_title }}</p>
           <!-- <p>lingua: <img :src="require('@/assets/' + movie.original_language + '.png' || '.jpg')" alt="">{{ flags[movie.original_language] }}</p> -->
-          <p>lingua: <img :src="flags[movie.original_language]" :alt="`${movie.original_language} flag`"></p>
-          <p>voto: {{ movie.vote_average }}</p>
+          <p>
+            <strong>lingua originale:</strong>
+            <img
+              :src="
+                !flags[movie.original_language]
+                  ? urlGeneric
+                  : flags[movie.original_language]
+              "
+              :alt="`${movie.original_language} flag`"
+            />
+          </p>
+          <p><strong>voto:</strong> {{ movie.vote_average }}</p>
+        </li>
+      </ul>
+
+      <ul>
+        <h2>TV Shows</h2>
+        <li v-for="(show, i) in tvShows" :key="i">
+          <p><strong>titolo:</strong> {{ show.name }}</p>
+          <p><strong>titolo originale:</strong> {{ show.original_name }}</p>
+          <!-- <p>lingua: <img :src="require('@/assets/' + show.original_language + '.png' || '.jpg')" alt="">{{ flags[show.original_language] }}</p> -->
+          <p>
+            <strong>lingua originale:</strong>
+            <img
+              :src="
+                !flags[show.original_language]
+                  ? urlGeneric
+                  : flags[show.original_language]
+              "
+              :alt="`${show.original_language} flag`"
+            />
+          </p>
+          <p><strong>voto:</strong> {{ show.vote_average }}</p>
         </li>
       </ul>
     </div>
@@ -30,20 +62,19 @@ export default {
   data() {
     return {
       apiKey: "cd46b1fc41160a5e0f5ace06fece242a",
+      urlBaseMovie: "https://api.themoviedb.org/3/search/movie?",
+      urlBaseTvShow: "https://api.themoviedb.org/3/search/tv?",
       movies: [],
+      tvShows: [],
       flags: {
-        en: require("@/assets/en.png")/* '@/assets/en.png' */,
-        es: require("@/assets/es.png")/* '@/assets/es.png' */,
-        fr: require("@/assets/fr.jpg")/* '@/assets/fr.jpg' */,
-        it: require("@/assets/it.png")/* '@/assets/it.png' */
+        en: require("@/assets/en.png") /* '@/assets/en.png' */,
+        es: require("@/assets/es.png") /* '@/assets/es.png' */,
+        fr: require("@/assets/fr.jpg") /* '@/assets/fr.jpg' */,
+        it: require("@/assets/it.png") /* '@/assets/it.png' */,
       },
+      urlGeneric: require("@/assets/notfound.jpg"),
       searchedString: "",
     };
-  },
-  computed: {
-   /*  singleFlag(){
-      return this.flags
-    } */
   },
   methods: {
     //eseguo la richiesta da axios all'api e popolo l'array "movies" ogni volta che premo enter o clicco su cerca
@@ -51,10 +82,36 @@ export default {
       //svuoto l'array all'inizio della funzione
       this.movies = [];
 
+      //PER I FILM
       //eseguo la chiamata axios
       axios
         //come primo elemento della funzione get metto la prima parte necessaria per la ricerca di films (data dalla documentazione dell'api)
-        .get("https://api.themoviedb.org/3/search/movie?", {
+        .get(this.urlBaseMovie, {
+          //come secondo elemento del get inserisco params (un oggetto nel quale vengono specificati dei parametri, axios "traduce" questi parametri in stringa di ricerca)
+          params: {
+            //this.apiKey è la chiave data dall'api
+            api_key: this.apiKey,
+            //this.searchedString è un v-model inserito nell'input di testo, query è il secondo parametro necessario per la ricerca dei film
+            query: this.searchedString,
+            language: "it",
+          },
+        })
+        .then((resp) => {
+          //prendo l'array di risposta dato dall'api e lo inseriso nel mio array movies
+          this.movies.push(...resp.data.results);
+        });
+      console.log("array di film", this.movies);
+    
+
+    
+      //svuoto l'array all'inizio della funzione
+      this.tvShows = [];
+
+      //PER LE SERIE
+      //eseguo la chiamata axios
+      axios
+        //come primo elemento della funzione get metto la prima parte necessaria per la ricerca di films (data dalla documentazione dell'api)
+        .get(this.urlBaseTvShow, {
           //come secondo elemento del get inserisco params (un oggetto nel quale vengono specificati dei parametri, axios "traduce" questi parametri in stringa di ricerca)
           params: {
             //this.apiKey è la chiave data dall'api
@@ -65,28 +122,16 @@ export default {
         })
         .then((resp) => {
           //prendo l'array di risposta dato dall'api e lo inseriso nel mio array movies
-          this.movies.push(...resp.data.results);
+          this.tvShows.push(...resp.data.results);
         });
-      console.log("array di film", this.movies);
-    },
-  },
-  mounted() {
-    /* axios
-    .get("https://api.themoviedb.org/3/search/movie?", {
-      params: {
-        api_key: this.apiKey,
-        query: this.searchedString
-    }})
-    .then(resp => {
-      this.movies.push(...resp.data.results) 
-    });
-    console.log("array di prova", this.movies); */
-  },
+      console.log("array di tv shows", this.tvShows);
+    }
+  }
 };
 </script>
 
 <style lang="scss">
-@import 'styles/app.scss';
+@import "styles/app.scss";
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
