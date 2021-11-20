b9c4a147b9789a3e546f802364d5293eb408e0eb
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-11-20 17:02:21 +0100

    removes li tag

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,37 +1,35 @@
 <template>
   <div class="single_card">
-    <li>
-      <p><strong>titolo:</strong> {{ title || name }}</p>
-      <p>
-        <strong>titolo originale:</strong> {{ original_title || original_name }}
-      </p>
-      <p>
-        <strong>lingua originale:</strong>
-        <img
-          :class="flags[original_language] ? 'flag_image' : 'lang_not_found'"
-          :src="
-            !flags[original_language] ? urlGeneric : flags[original_language]
-          "
-          :alt="`${original_language} flag`"
-        />
-        <span v-if="!flags[original_language]">{{ original_language }}</span>
-      </p>
-      <p>
-        <strong>voto:</strong>
-        <i
-          class="fa-star"
-          v-for="number in 5"
-          :key="number"
-          :class="number > getVoteFilms ? 'far' : 'fas'"
-        ></i>
-      </p>
-      <div class="poster">
-        <img
-          :src="urlBasePoster + 'w342' + poster_path"
-          :alt="title + ' poster'"
-        />
-      </div>
-    </li>
+    <p><strong>titolo:</strong> {{ title || name }}</p>
+    <p>
+      <strong>titolo originale:</strong> {{ original_title || original_name }}
+    </p>
+    <p>
+      <strong>lingua originale:</strong>
+      <img
+        :class="flags[original_language] ? 'flag_image' : 'lang_not_found'"
+        :src="
+          !flags[original_language] ? urlGeneric : flags[original_language]
+        "
+        :alt="`${original_language} flag`"
+      />
+      <span v-if="!flags[original_language]">{{ original_language }}</span>
+    </p>
+    <p>
+      <strong>voto:</strong>
+      <i
+        class="fa-star"
+        v-for="number in 5"
+        :key="number"
+        :class="number > getVoteFilms ? 'far' : 'fas'"
+      ></i>
+    </p>
+    <div class="poster">
+      <img
+        :src="urlBasePoster + 'w342' + poster_path"
+        :alt="title + ' poster'"
+      />
+    </div>
   </div>
 </template>
 
