f4e926c1b38255a5ef23777f67dab8e7c798184b
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-11-20 18:33:54 +0100

    adds a search bar container

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="navbar">
+    <div class="logo_title">
       <h1>BOOLFLIX</h1>
+    </div>
+    <div class="search_bar_container">
+        <!-- qui ci sarà la barra di ricerca -->
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Navbar'
-}
+  name: "Navbar",
+};
 </script>
 
 <style>
-
 </style>
